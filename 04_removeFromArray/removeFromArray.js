a8150e0ff261ca8fa10fd653f812f18e443659bf
Bonevane <114078747+Bonevane@users.noreply.github.com>
2024-09-02 01:32:31 +0500

    Initial Commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    for(let i = 0; i < args.length; i++){
+        array = array.filter(function(letter) {
+            return !args.includes(letter);
+        });
+    }
+
+    return array;
 };
 
 // Do not edit below this line
