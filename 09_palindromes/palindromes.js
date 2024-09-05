da027d6f99809b27050ecd5bfcbf22e0bfb5712c
Bonevane <114078747+Bonevane@users.noreply.github.com>
2024-09-06 01:13:17 +0500

    Finished Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const processedString = string.toLowerCase().split('').filter((char) => alphanumerical.includes(char)).join('');
+    
+    const reversedString = processedString.split('').reverse().join('');
+
+    return processedString == reversedString;
 };
 
 // Do not edit below this line
