8a95d56ce5b4058e39df1fef853c3c7369a74c0c
Bonevane <114078747+Bonevane@users.noreply.github.com>
2024-09-06 01:40:21 +0500

    Finished Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    const titles = [];
 
+    arr.forEach(element => {
+        titles.push(element.title);
+    });
+
+    return titles;
 };
 
 // Do not edit below this line
